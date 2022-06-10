33b6e32fcca39f226cadcbdad1a630269f8a28dd
Sanju R <Sanju R@DESKTOP-CAOVE2E>
2022-06-10 11:18:37 +0530

    hi

--- Test/src/module-info.java
+++ Test/src/module-info.java
@@ -1,2 +1,21 @@
-module Test {
+class Main {
+  public static void main(String[] args) {
+
+    int num = 1234, reversed = 0;
+    
+    System.out.println("Original Number: " + num);
+
+    // run loop until num becomes 0
+    while(num != 0) {
+    
+      // get last digit from num
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+     //hello demo 
+      // remove the last digit from num
+      num /= 10;
+    }
+
+    System.out.println("Reversed Number: " + reversed);
+  }
 }
